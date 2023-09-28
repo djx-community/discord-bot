512acc14e2034f6803a19ca99726e1dec896d003
Naveenravi07 <gamerkidnav@gmail.com>
2023-09-28 15:57:54 +0530

    Env added

--- src/database/database.js
+++ src/database/database.js
@@ -1,8 +1,8 @@
-
+require('dotenv').config()
 const mongoose = require('mongoose')
 
 const connectDb = async () => {
-    const connection = await mongoose.connect(`mongodb://localhost:27017`)
+    const connection = await mongoose.connect(process.env.mongo_uri?process.env.mongo_uri:"")
     switch(connection.connection.readyState){
         case 0: console.log(`Connection to database failed`); break
         case 1: console.log(`Connection to databse successful`); break
